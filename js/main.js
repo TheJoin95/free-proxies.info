6a1da3acf4cf55977483f0b2d0319f1fcbcf0d81
thejoin95 <miki.lombi@gmail.com>
2017-05-17 21:20:55 +0200

    added fb and tw meta

--- js/main.js
+++ js/main.js
@@ -11,6 +11,52 @@ $(document).ready(function(){
 	});
 
 
+	if($('#userForm').length > 0) {
+		$.ajax({
+			url: "https://api.free-proxies.info/user/get",
+			dataType: "JSONP",
+			contentType: "JSON",
+			success: function(data){
+				if(data != null) {
+					$('#alreadySigned, #service').fadeIn('slow');
+					$('#user_id').val(data["_id"]);
+					$('#user_api_key').val(data["api_key"]).attr('readonly', 'readonly');
+					$('#user_type').val(data["type"]).attr('readonly', 'readonly');
+					$('#user_name').val(data["name"]);
+					$('#host').val(data["host"]);
+					setTimeout(function() {
+						$('html,body').animate({
+				          scrollTop: $('#user_api_key').offset().top - 20
+				        }, 300);
+					}, 700);
+					// if(typeof(data["requests"]) !== "undefined") {
+					// 	var today = new Date();
+					// 	var key = today.getFullYear() + "-" + (today.getMonth()+1) + "-" + today.getDate();
+					// 	$('#requestDay').text(data['requests'][key]).counterUp({
+					// 										        delay: 10,
+					// 										        time: 1000
+					// 										    });
+					// }
+				}else{
+					$('#needSignin').fadeIn('slow');
+				}
+			}
+		});
+
+		$('#updateUser').on('click', function(e) {
+			e.preventDefault();
+			var formData = $("#userForm").serializeArray();
+			$.ajax({
+				url: "https://api.free-proxies.info/user/update",
+				method:"POST",
+				data: formData,
+				success: function(data){
+					console.log(data);
+					window.location.reload();
+				}
+			});
+		});
+	}
 
 
 	$("#clients-logo").owlCarousel({
